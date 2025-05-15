b4fdef1ce4de9152b73bd1a5dec34c719324a0e2
(박서연) <(julliet0915@naver.com)>
2025-05-15 09:47:03 +0900

    게시물생성과조회

---
+++ server.js
@@ -0,0 +1,97 @@
+const mongoclient = require('mongodb').MongoClient;
+const url =
+'mongodb+srv://eeeon:0915@cluster0.oz5ftkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+let mydb;
+mongoclient.connect(url)
+    .then(client => {
+        mydb = client.db('myboard');
+        mydb.collection('post').find().toArray().then(result => {
+            console.log(result);
+        })
+        
+
+        app.listen(8080, function(){
+            console.log("포트 8080으로 서버 대기중 ... ")
+        });
+    }).catch(err => {
+        console.log(err);
+    });
+    console.log('몽고DB 접속 성공');
+
+
+// MySQL + Node.js 접속 코드
+var mysql = require("mysql");
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "0915",
+    database: "myboard",
+});
+
+conn.connect();
+
+const express = require('express');
+const app = express();
+
+// body-parser 라이브러리 추가
+const bodyParser = require('body-parser');
+
+
+//템플릿 엔진 ejs 관련 코드 추가
+const db = require('node-mysql/lib/db');app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+
+
+// /list 요청 시 데이터 조회 코드
+app.get('/list', function(req, res){
+    // 몽고DB연결 시 주석 처리하기기
+    // conn.query("select * from post", function (err, rows, fields) {
+    //     if (err) throw err;
+    //     console.log(rows);
+    // });
+    mydb.collection('post').find().toArray(function(err, result){
+        console.log(result);       
+        // ejs 사용 코드
+        res.render('list.ejs', {data : result});
+    })
+    // ejs 사용 전 데이터베이스 연결 코드
+    // res.sendFile(__dirname + '/list.html');
+
+
+
+    // console.log('데이터베이스를 조회합니다.');
+
+});
+
+// '/enter' 요청에 대한 처리 루틴
+app.get('/enter', function(req, res){
+    res.sendFile(__dirname + '/enter.html');
+});
+
+// '/save' 요청에 대한 post 방식의 처리 루틴
+app.post('/save', function(req, res){
+    console.log(req.body.title);
+    console.log(req.body.content);
+    console.log(req.body.someDate);
+
+    //몽고DB에 데이터 저장하기
+    mydb.collection('post').insertOne(
+        {title : req.body.title, content : req.body.content, date : req.body.someDate}
+    ).then(result => {
+        console.log(result);
+        console.log('데이터 추가 성공');
+    });
+
+    //MySQL DB에 데이터 저장하기
+    // let sql = "insert into post (title, content, created) values(?, ?, NOW())";
+    // let params = [req.body.title, req.body.content];
+    // conn.query(sql, params, function (err, result) {
+    //     if (err) throw err;
+    //     console.log('데이터 추가 성공');
+    // });
+    res.send('데이터 추가 성공');
+    // console.log("저장완료");
+});
+
+// 서버 주소: http://localhost:8080/enter
